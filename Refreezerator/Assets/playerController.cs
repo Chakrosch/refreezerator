6cda9469dba38bfaf5a69381f57fa8b49e2c8fc3
ge35mit <ge35mit@mytum.de>
2019-12-06 21:26:24 +0100

    Initial Commit

---
+++ Refreezerator/Assets/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed;
+    private Vector3 movement; // y axis should always be zero
+
+    // Start is called before the first frame update
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+        move();
+    }
+
+    private void getInput()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+    }
+
+    private void move()
+    {
+        transform.position += movement; 
+    }
+}
