a9b353a6fd5489977f5c0878cb57615d4c3204e4
Saman Miran <s.miran33@gmail.com>
2019-12-07 20:16:50 +0100

    Pixel shader working

---
+++ Refreezerator/Assets/Scripts/ShaderCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderCam : MonoBehaviour
+{
+	public Material postProcessingMat;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+	
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		postProcessingMat.SetFloat("_OrthoSize", Camera.main.orthographicSize);
+		Graphics.Blit(source, destination, postProcessingMat);
+	}
+}
