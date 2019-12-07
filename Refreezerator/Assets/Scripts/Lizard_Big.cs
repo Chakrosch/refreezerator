5a03e3ce8f145ee0e3965a76e1ef3620cd1c31ec
Benedikt Wiberg <benedikt.wiberg@gmail.com>
2019-12-07 20:34:43 +0100

    Fix lizard

--- Refreezerator/Assets/Scripts/Lizard_Big.cs
+++ Refreezerator/Assets/Scripts/Lizard_Big.cs
@@ -4,148 +4,154 @@ using UnityEngine;
 
 public class Lizard_Big : MonoBehaviour
 {
-private float direction_change_timer = 5f;
-public Rigidbody rb;
-public float speed;
-public int chaseDistance;
-private bool playerNearby= false;
-public GameObject player;
-private int layerMask = 1 << 10; 
+    private float direction_change_timer = 5f;
+    public Rigidbody rb;
+    public float speed;
+    public int chaseDistance;
+    private bool playerNearby = false;
+    public GameObject player;
+    private int layerMask = 1 << 10;
 
 
 // Start is called before the first frame update
-void Start()
-{
-    layerMask = ~layerMask; //bitshift for collision with everything except layer 10 (player)
-}
+    void Start()
+    {
+        layerMask = ~layerMask; //bitshift for collision with everything except layer 10 (player)
+    }
 
 // Update is called once per frame
-void Update()
-{
-updatePlayerDistance();
-if (playerNearby)
-{
-    ChaseMove();
-}
-else
-{
-    IdleMove();
-    
-}
-}
-
-void ChaseMove()
-{
-    bool walkAround = false;
-    Vector3 dir = player.transform.position - this.transform.position;
-  // Debug.DrawRay( transform.position+new Vector3(0,0,0.5f),  dir,  Color.green,   1.0f,  false);
-    if (Physics.Raycast(transform.position+new Vector3(0,0,0.5f), dir, 2, layerMask)
-        ||Physics.Raycast(transform.position-new Vector3(0,0,0.5f), dir, 2, layerMask)
-        ||Physics.Raycast(transform.position+new Vector3(0.5f,0,0), dir, 2, layerMask)
-        ||Physics.Raycast(transform.position+new Vector3(0.5f,0,0), dir, 2, layerMask))
+    void Update()
     {
-     
-
-    Vector3 movement = new Vector3(
-        player.transform.position.x - this.transform.position.x,
-        player.transform.position.y - this.transform.position.y,
-        player.transform.position.z - this.transform.position.z
-    );
-    movement = Vector3.Normalize(movement);
-    if (walkAround)
-    {
-       movement = WalkAround();
+        updatePlayerDistance();
+        if (playerNearby)
+        {
+            ChaseMove();
+        }
+        else
+        {
+            IdleMove();
+        }
     }
 
-rb.velocity = movement * speed;
-}
+    void ChaseMove()
+    {
+        bool walkAround = false;
+        Vector3 dir = player.transform.position - this.transform.position;
+        // Debug.DrawRay( transform.position+new Vector3(0,0,0.5f),  dir,  Color.green,   1.0f,  false);
+        if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), dir, 2, layerMask)
+            || Physics.Raycast(transform.position - new Vector3(0, 0, 0.5f), dir, 2, layerMask)
+            || Physics.Raycast(transform.position + new Vector3(0.5f, 0, 0), dir, 2, layerMask)
+            || Physics.Raycast(transform.position + new Vector3(0.5f, 0, 0), dir, 2, layerMask))
+        {
+            Vector3 movement = new Vector3(
+                player.transform.position.x - this.transform.position.x,
+                player.transform.position.y - this.transform.position.y,
+                player.transform.position.z - this.transform.position.z
+            );
+            movement = Vector3.Normalize(movement);
+            if (walkAround)
+            {
+                movement = WalkAround();
+            }
 
-void IdleMove()
-{
-        
-if (direction_change_timer >= 5f)
-{
-    direction_change_timer = 0f;
-    int dir = Random.Range(0, 4);
-
-    if (dir == 0)
-    {
-        rb.velocity = new Vector3(0, 0, 0);
-    }else if (dir == 1)
-    {
-        rb.velocity = new Vector3(1, 0, 1)*speed;
-    }else if (dir == 2)
-    {
-        rb.velocity = new Vector3(-1, 0, 1)*speed;
-    }else if (dir == 3)
-    {
-        rb.velocity = new Vector3(-1, 0, 1)*speed;
-    }else 
-    {
-        rb.velocity = new Vector3(-1, 0, -1)*speed;
+            rb.velocity = movement * speed;
+        }
     }
 
-}
+    void IdleMove()
+        {
+            if (direction_change_timer >= 5f)
+            {
+                direction_change_timer = 0f;
+                int dir = Random.Range(0, 4);
 
-direction_change_timer+= Time.deltaTime;
-}
-private void updatePlayerDistance()
-{
-    
-if(Vector3.Distance(player.transform.position, this.transform.position)<chaseDistance)
-{
-    playerNearby = true;
-}
-else {
-    playerNearby = false;
-}
-}
+                if (dir == 0)
+                {
+                    rb.velocity = new Vector3(0, 0, 0);
+                }
+                else if (dir == 1)
+                {
+                    rb.velocity = new Vector3(1, 0, 1) * speed;
+                }
+                else if (dir == 2)
+                {
+                    rb.velocity = new Vector3(-1, 0, 1) * speed;
+                }
+                else if (dir == 3)
+                {
+                    rb.velocity = new Vector3(-1, 0, 1) * speed;
+                }
+                else
+                {
+                    rb.velocity = new Vector3(-1, 0, -1) * speed;
+                }
+            }
 
-Vector3 WalkAround()
-{
-    float a = player.transform.position.x - this.transform.position.x;
-    float b = player.transform.position.y - this.transform.position.y;
-    //Bestimmt die beiden Richtungen in der sich d Player befindet. Raycastet nach der blockierenden wand und läuft in die andere Richtung.
-    if (a > 0 && b > 0)
-    {
-        if (Physics.Raycast(transform.position+new Vector3(0,0,0.5f), new Vector3(1,0,0), 2, layerMask))
-        {
-            Debug.Log("oben");
-            return new Vector3(0,0,1);
+            direction_change_timer += Time.deltaTime;
         }
 
-        Debug.Log("rechts1");
-        return new Vector3(1,0,0);
-    }else if (a <= 0 && b > 0)
-    {
-        if (Physics.Raycast(transform.position+new Vector3(0,0,0.5f), new Vector3(-1,0,0), 2, layerMask))
+    private void updatePlayerDistance()
         {
-            Debug.Log("links");
-            return new Vector3(-1,0,0);
+            if (Vector3.Distance(player.transform.position, this.transform.position) < chaseDistance)
+            {
+                playerNearby = true;
+            }
+            else
+            {
+                playerNearby = false;
+            }
         }
-        Debug.Log("oben");
-        return new Vector3(0,0,1);
-    }else if (a > 0 && b <= 0)
-    {
-        if (Physics.Raycast(transform.position+new Vector3(0,0,0.5f), new Vector3(1,0,0), 2, layerMask))
+
+        Vector3 WalkAround()
         {
-            Debug.Log("unten");
-            return new Vector3(0,0,-1);
+            float a = player.transform.position.x - this.transform.position.x;
+            float b = player.transform.position.y - this.transform.position.y;
+            //Bestimmt die beiden Richtungen in der sich d Player befindet. Raycastet nach der blockierenden wand und läuft in die andere Richtung.
+            if (a > 0 && b > 0)
+            {
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(1, 0, 0), 2, layerMask))
+                {
+                    Debug.Log("oben");
+                    return new Vector3(0, 0, 1);
+                }
+
+                Debug.Log("rechts1");
+                return new Vector3(1, 0, 0);
+            }
+            else if (a <= 0 && b > 0)
+            {
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(-1, 0, 0), 2, layerMask))
+                {
+                    Debug.Log("links");
+                    return new Vector3(-1, 0, 0);
+                }
+
+                Debug.Log("oben");
+                return new Vector3(0, 0, 1);
+            }
+            else if (a > 0 && b <= 0)
+            {
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(1, 0, 0), 2, layerMask))
+                {
+                    Debug.Log("unten");
+                    return new Vector3(0, 0, -1);
+                }
+
+                Debug.Log("rechts2");
+                return new Vector3(1, 0, 0);
+            }
+            else if (a <= 0 && b <= 0)
+            {
+                if (Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), new Vector3(-1, 0, 0), 2, layerMask))
+                {
+                    Debug.Log("unten");
+                    return new Vector3(0, 0, -1);
+                }
+
+                Debug.Log("links");
+                return new Vector3(-1, 0, 0);
+            }
+
+            return new Vector3(0, 10, 0); //sollte nie erreicht werden, wenn doch fliegt biggus echsus jetzt zum Mond
         }
-        
-        Debug.Log("rechts2");
-        return new Vector3(1,0,0);
-    }else if (a <= 0 && b <= 0){
-    
-        if (Physics.Raycast(transform.position+new Vector3(0,0,0.5f),  new Vector3(-1,0,0),2, layerMask))
-        {
-            Debug.Log("unten");
-            return new Vector3(0,0,-1);
-        }
-        
-        Debug.Log("links");
-    return new Vector3(-1,0,0);
     }
-    return new Vector3(0,10,0); //sollte nie erreicht werden, wenn doch fliegt biggus echsus jetzt zum Mond
-}
-}
