f6c10e8f076bc00b7576fd9b8db4a7e251dd1060
ge35mit <ge35mit@mytum.de>
2019-12-07 22:15:57 +0100

    no message

---
+++ Refreezerator/Assets/icicleSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class icicleSlider : MonoBehaviour
+{
+    public Vector2 startPos;
+    public Vector2 endPos;
+    RectTransform rectTransform;
+    public float value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition = Vector3.Lerp(startPos, endPos, value);
+    }
+}
