c20329ef7d172cb6a1b19e97954599fbc02068d3
chakrosch <georg.eickelpasch@gmx.de>
2019-12-08 10:35:34 +0900

    der BofrostMachine.cs kommt

---
+++ Refreezerator/Assets/Scripts/BofrostMachine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BofrostMachine : MonoBehaviour
+{
+    public GameObject prefab;
+	public List<GameObject> allTheVeggies = new List<GameObject>();
+public float spawningRate;
+private float currentSpawnTimer = 0;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	currentSpawnTimer = currentSpawnTimer+ Time.deltaTime;
+if(currentSpawnTimer >= spawningRate){
+        GameObject go = (GameObject) Instantiate(prefab, new Vector3(0 , 5, 0), Quaternion.identity);
+		currentSpawnTimer = 0 + Random.Range(0,5);
+}
+        
+    }
+}
