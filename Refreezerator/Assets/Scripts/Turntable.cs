a39b0e878a4da5161b31cf9689a0d9efb7cb3646
Steffen Florschuetz <ge93fir@mytum.de>
2019-12-08 04:23:39 +0100

    Hauptmenü

---
+++ Refreezerator/Assets/Scripts/Turntable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turntable : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0,Time.deltaTime * speed,0));
+    }
+}
