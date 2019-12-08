3e2381140f49c1fbe770e2da285adfd4e4f9d850
chakrosch <georg.eickelpasch@gmx.de>
2019-12-08 10:21:44 +0900

    BofrostMachine.cs INTRODUKTIZIERTUNG

--- Refreezerator/Assets/Scripts/Vegetable.cs
+++ Refreezerator/Assets/Scripts/Vegetable.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Vegetable : PickUpObject
 {
+    
 
-    public enum vegetables
+public enum vegetables
     {
         carrot,
         broccoli,
@@ -14,13 +15,27 @@ public class Vegetable : PickUpObject
         tomato,
         paprika
     }
+public vegetables type;
 
+public  Sprite carrotS;
 
-    public vegetables type;
+public  Sprite broccoliS;
+
+public  Sprite eggplantS;
+
+public Sprite cornS;
+
+public Sprite tomatoS;
+
+public Sprite paprikaS;
     // Start is called before the first frame update
     new void Start()
     {
+        carrotS = Resources.Load<Sprite>("carrot");
         base.Start();
+        type = RandomVegetable();
+        Sprite vegSprite = TypeToSprite(type);
+        gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = vegSprite;
     }
 
     // Update is called once per frame
@@ -42,5 +57,54 @@ public class Vegetable : PickUpObject
         }
 
     }
+   private vegetables RandomVegetable ()
+    {
+        int i = Random.Range(0, 5);
+        switch (i)
+        {
+            case 0:
+                return vegetables.carrot;
+                
+            case 1:
+                return vegetables.broccoli;
+               
+            case 2:
+                return vegetables.eggplant;
+                
+            case 3:
+                return vegetables.corn;
+                
+            case 4:
+                return vegetables.tomato;
+               
+            case 5:
+                return vegetables.paprika;
+               
+            default:
+                return vegetables.carrot;
+              
+        }
+    }
 
+   private Sprite TypeToSprite(vegetables veg)
+   {
+       switch (veg)
+       {
+           case vegetables.carrot:
+               return carrotS;
+           case vegetables.broccoli:
+               return broccoliS;
+           case vegetables.eggplant:
+               return eggplantS;
+           case vegetables.corn:
+               return cornS;
+           case vegetables.tomato:
+               return tomatoS;
+           case vegetables.paprika:
+               return paprikaS;
+
+           default:
+               return null;
+       }
+   }
 }
