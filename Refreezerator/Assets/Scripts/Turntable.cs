83555f6dc088ec79a59de50498768eb8e3552065
Steffen Florschuetz <ge93fir@mytum.de>
2019-12-08 07:25:12 +0100

    Better Main Menu

--- Refreezerator/Assets/Scripts/Turntable.cs
+++ Refreezerator/Assets/Scripts/Turntable.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Turntable : MonoBehaviour
 {
     public Vector3 speed = new Vector3(0f,1f,0f);
+    public bool bounce = false;
 
     void Update()
     {
-        this.transform.Rotate(Time.deltaTime*speed);
+        if (bounce)
+            this.transform.Rotate(Mathf.Sin(Time.time) * speed);
+        else
+            this.transform.Rotate(Time.deltaTime*speed);
     }
 }
