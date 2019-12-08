5c7b6b016749e83ab1c9366583b9607b300ed19c
chakrosch <georg.eickelpasch@gmx.de>
2019-12-08 19:14:15 +0900

    forgot to add lelelelelelle

---
+++ Refreezerator/Assets/Scripts/Quicksand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quicksand : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            Destroy(other.gameObject);
+        }
+        else
+        {
+            PlayerController.GameOver();
+        }
+    }
+}
